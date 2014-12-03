3e37882143c43d893d9ca3409beca36473214a9e
Alexander Grabanski <spagrab@gmail.com>
2014-12-02 20:44:05 -0500

    Prettied up the UI a bit

--- UI.py
+++ UI.py
@@ -4,22 +4,21 @@ from S3PointMass import *
 import wx
 
 #Create the main window
-L = 320
-Hregion = 400
-w = window(width=2*(L+window.dwidth), height=L+window.dheight+Hregion,
+L = 600
+w = window(width=1.5*(L+window.dwidth), height=L+window.dheight,
             menus = False, title='Noneuclidean Gravity')
-d = 20
-disp = display(window=w, x=d, y=d, width = L-2*d, height=L-2*d)
+
+disp = display(window=w, x=20, y=20, width = L-40, height=L-40)
 
 p = w.panel
 
 
-minspeedrangeselector = wx.TextCtrl(p, pos=(1.1*L, 0.1*L), value='0', size=(50, 20))
-maxspeedrangeselector = wx.TextCtrl(p, pos=(1.4*L, 0.1*L), value='0', size=(50, 20))
+minspeedrangeselector = wx.TextCtrl(p, pos=(1.1*L, 0.1*L), value='0.0', size=(50, 20))
+maxspeedrangeselector = wx.TextCtrl(p, pos=(1.4*L, 0.1*L), value='5.0', size=(50, 20))
 wx.StaticText(p, pos=(1.2*L, 0.05*L), label='Speed range')
 
-minmassrangeselector = wx.TextCtrl(p, pos=(1.1*L, 0.2*L), value='0', size=(50, 20))
-maxmassrangeselector = wx.TextCtrl(p, pos=(1.4*L, 0.2*L), value='0', size=(50, 20))
+minmassrangeselector = wx.TextCtrl(p, pos=(1.1*L, 0.2*L), value='0.1', size=(50, 20))
+maxmassrangeselector = wx.TextCtrl(p, pos=(1.4*L, 0.2*L), value='2.0', size=(50, 20))
 wx.StaticText(p, pos=(1.2*L, 0.15*L), label='Mass range')
 
 gravselector = wx.TextCtrl(p, pos=(1.25*L, 0.3*L), value='0.3', size=(50,20))
